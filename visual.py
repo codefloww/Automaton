068c484913bb5fd9bffec7dd7816a30fba7f43ce
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-05-24 13:05:18 +0300

    objects __init__ + draw()

--- visual.py
+++ visual.py
@@ -1,30 +1,56 @@
 import pygame, sys
-DISPLAY_X = 1000
-DISPLAY_Y = 700
 
 
 class Object:
-    def __init__(self, x, y):
+    def __init__(self, x, y, screen):
         self.coordinate_x = x
         self.coordinate_y = y
+        self.screen = screen
 
 
 class Cell(Object):
-    pass
+    radius = 10
+
+    def __init__(self, x, y, screen, color):
+        super().__init__(x, y, screen)
+        self.color = color
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color,
+                           (self.coordinate_x, self.coordinate_y), Cell.radius)
 
 
 class Plant(Object):
-    pass
+    color = (80, 150, 80)
+    radius = 20
+
+    def __init__(self, x, y, screen):
+        super().__init__(x, y, screen)
+
+    def draw(self):
+        pygame.draw.circle(self.screen, Plant.color,
+                         (self.coordinate_x, self.coordinate_y), Plant.radius)
 
 
 class Wall(Object):
-    pass
+    color = (40, 40, 40)
+    size = (40, 40)
+
+    def __init__(self, x, y, screen):
+        super().__init__(x, y, screen)
+
+    def draw(self):
+        pygame.draw.rect(self.screen, Wall.color,
+                         (self.coordinate_x, self.coordinate_y, Wall.size[0], Wall.size[1]))
 
 
 class GUI:
+    DISPLAY_X = 1000
+    DISPLAY_Y = 700
+
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((DISPLAY_X, DISPLAY_Y))  # create a screen
+        self.screen = pygame.display.set_mode((GUI.DISPLAY_X, GUI.DISPLAY_Y))  # create a screen
         pygame.display.set_caption('Evolution Game')
         pygame.display.set_icon(pygame.image.load('../Automaton/images/evolution.png'))  # program image
 
@@ -41,6 +67,15 @@ class GUI:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         click = True
+
+            # приклади виведення об'єктів (над кольорами треба ще попрацювати :) )
+            wall1 = Wall(500, 100, self.screen)
+            wall1.draw()
+            plant1 = Plant(300, 300, self.screen)
+            plant1.draw()
+            cell1 = Cell(200, 200, self.screen, (70, 10, 70))
+            cell1.draw()
+
 
             pygame.display.update()
 
