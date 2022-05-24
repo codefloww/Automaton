8153cc751ce7fbfa5c9ba583fd58220fa157adfa
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-05-24 17:31:11 +0300

    demo buttons work

--- visual.py
+++ visual.py
@@ -1,4 +1,7 @@
+import random
+
 import pygame, sys
+
 
 class Object:
     def __init__(self, x, y, screen):
@@ -9,6 +12,7 @@ class Object:
     def move(self, x, y):
         self.coordinate_x = (self.coordinate_x + x) % GUI.DISPLAY_X
         self.coordinate_y = (self.coordinate_y + y) % GUI.DISPLAY_Y
+
 
 class Cell(Object):
     radius = 10
@@ -83,54 +87,52 @@ class GUI:
         self.screen = pygame.display.set_mode((GUI.DISPLAY_X, GUI.DISPLAY_Y))  # create a screen
         pygame.display.set_caption('Evolution Game')
         pygame.display.set_icon(pygame.image.load('images/evolution.png'))  # program image
-        #load button images
 
+        #load button images
+        pygame.draw.rect(self.screen, (30, 50, 50), pygame.Rect(900, 0, 100, 700))
         self.light_button = Button(930, 100, "images/idea.png")
         self.cell_button = Button(930, 200, "images/cell.png")
         self.wall_button = Button(930, 300, "images/wall.png")
         self.plant_button = Button(930, 400, "images/plant.png")
         self.play_button = Button(930, 500, "images/play-button.png")
 
+        self.cells = []
+        self.plants = []
+        self.walls = []
+
     def spawn_cell(self):
         cell1 = Cell(200, 200, self.screen, (70, 10, 70))
         cell1.draw()
 
     def main(self):
-        while True:
+
+        run = True
+        while run:
             self.screen.fill((70, 80, 80))
-            mx, my = pygame.mouse.get_pos()
-            click = False
-            color = (255,255,255)
-            pygame.draw.rect(self.screen, color, pygame.Rect(900, 0, 100, 700))
+            pygame.draw.rect(self.screen, (30, 50, 50), pygame.Rect(900, 0, 100, 700))
+
     
             if self.light_button.draw(self.screen):
                 print("Turn the light on")
             if self.cell_button.draw(self.screen):
-                print("Spawn a cell")
+                # print("Spawn a cell")
+                self.cells.append(Cell(random.randint(0, GUI.DISPLAY_X-100), random.randint(0, GUI.DISPLAY_Y), self.screen, (70, 10, 70)))
             if self.plant_button.draw(self.screen):
-                print("Place some food")
+                # print("Place some food")
+                self.plants.append(Plant(random.randint(0, GUI.DISPLAY_X-100), random.randint(0, GUI.DISPLAY_Y), self.screen))
             if self.play_button.draw(self.screen):
                 print("Evolution starts")
             if self.wall_button.draw(self.screen):
-                print("Build a wall")
-
+                # print("Build a wall")
+                self.walls.append(Wall(random.randint(0, GUI.DISPLAY_X-100), random.randint(0, GUI.DISPLAY_Y), self.screen))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:  # if press close button
+                    run = False
                     pygame.quit()
 
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
-
-            # приклади виведення об'єктів (над кольорами треба ще попрацювати :) )
-            wall1 = Wall(500, 100, self.screen)
-            wall1.draw()
-            plant1 = Plant(300, 300, self.screen)
-            plant1.draw()
-            cell1 = Cell(200, 200, self.screen, (70, 10, 70))
-            cell1.draw()
-
+            for obj in [*self.cells, *self.plants, *self.walls]:
+                obj.draw()
             pygame.display.update()
 
 
