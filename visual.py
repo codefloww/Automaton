ddf7fb8e6c0f9886f3ecbbf207a0a202e787b138
YarynaFialko <92580927+YarynaFialko@users.noreply.github.com>
2022-06-04 22:13:53 +0300

    quit fixed + erase button

--- visual.py
+++ visual.py
@@ -42,6 +42,7 @@ class Plant(Cell):
         pygame.draw.circle(GUI.SCREEN, Plant.color,
                            (self.x, self.y), Plant.radius, 4)
 
+
 class Wall(Cell):
     color = (51, 53, 53)
     size = (11, 11)
@@ -117,40 +118,31 @@ class GUI:
     TEXT_COLOR = (10, 20, 10)
     SCREEN = pygame.display.set_mode((DISPLAY_X, DISPLAY_Y))
 
-    def __init__(self, env = None):
+    def __init__(self):
         pygame.init()
-        pygame.display.set_caption("Evolution Game")
-        pygame.display.set_icon(
-            pygame.image.load("images/evolution.png")
-        )  # program image
+        pygame.display.set_caption('Evolution Game')
+        pygame.display.set_icon(pygame.image.load('images/evolution.png'))  # program image
         pygame.font.init()
         self.font = pygame.font.SysFont(GUI.TEXT_FONT, GUI.TEXT_SIZE)
-        
         self.display_color = GUI.DISPLAY_COLOR_NIGHT
         self.menu_color = GUI.MENU_COLOR_NIGHT
 
         # load button images
-        self.light_button = Button(940, 80, "images/idea.png", "images/idea_on.png")
-        self.cell_button = Button(
-            940, 180, "images/virus.png", "images/virus_on.png", "Cell"
-        )
-        self.wall_button = Button(
-            940, 280, "images/wall.png", "images/wall_on.png", "Wall"
-        )
-        self.plant_button = Button(
-            940, 380, "images/plant.png", "images/plant_on.png", "Plant"
-        )
-        self.play_button = Button(
-            940, 480, "images/play-button.png", "images/video-pause-button.png"
-        )
-        self.quit_button = Button(940, 580, "images/remove.png")
+        self.light_button = Button(940, 50, "images/idea.png", "images/idea_on.png")
+        self.cell_button = Button(940, 140, "images/virus.png", "images/virus_on.png", "Cell")
+        self.wall_button = Button(940, 230, "images/wall.png", "images/wall_on.png", "Wall")
+        self.plant_button = Button(940, 320, "images/plant.png", "images/plant_on.png", "Plant")
+        self.erase_button = Button(940, 410, "images/eraser.png", "images/eraser_on.png")
+        self.play_button = Button(940, 500, "images/play-button.png", "images/video-pause-button.png")
+        self.quit_button = Button(940, 590, "images/remove.png")
         self.button = None
         self.cur_spawning_button = None
 
-
-        self.environment = env or Environment((GUI.DISPLAY_X - GUI.MENU_SIZE) // 7 + 1, GUI.DISPLAY_Y // 7 + 1)
+        self.environment = Environment((GUI.DISPLAY_X - GUI.MENU_SIZE) // 7 + 1, GUI.DISPLAY_Y // 7 + 1)
         self.coeff = 12
-
+        self.not_available_field = []
+        self.light = False
+        self.run = True
 
     def spawn_cell(self, name_class):
         for event in pygame.event.get():
@@ -171,6 +163,8 @@ class GUI:
                             y_c = (y // self.coeff) * self.coeff + self.coeff*j
                             if x_c <= GUI.DISPLAY_X - GUI.MENU_SIZE - Wall.size[0]:
                                 self.environment.set_cell(x_c // self.coeff, y_c // self.coeff, Wall(x_c, y_c))
+            if event.type == pygame.QUIT:  # if press close button
+                self.run= False
 
     def button_navigate(self, button):
         self.button = button
@@ -185,16 +179,10 @@ class GUI:
             self.spawn_cell(self.button.created_object)
 
     def change_light(self):
-        self.display_color = (
-            GUI.DISPLAY_COLOR_DAY
-            if self.display_color == GUI.DISPLAY_COLOR_NIGHT
-            else GUI.DISPLAY_COLOR_NIGHT
-        )
-        self.menu_color = (
-            GUI.MENU_COLOR_DAY
-            if self.menu_color == GUI.MENU_COLOR_NIGHT
-            else GUI.MENU_COLOR_NIGHT
-        )
+        self.display_color = GUI.DISPLAY_COLOR_DAY \
+            if self.display_color == GUI.DISPLAY_COLOR_NIGHT else GUI.DISPLAY_COLOR_NIGHT
+        self.menu_color = GUI.MENU_COLOR_DAY \
+            if self.menu_color == GUI.MENU_COLOR_NIGHT else GUI.MENU_COLOR_NIGHT
         self.light = not self.light
 
     def available_coordinates(self, x, y, radius):
@@ -206,8 +194,8 @@ class GUI:
 
     def main(self):
         time_start = time.time()
-        run = True
-        while run:
+        i = 0
+        while self.run:
             GUI.SCREEN.fill(self.display_color)
             for width in self.environment.grid:
                 for cell in width:
@@ -216,13 +204,12 @@ class GUI:
             pygame.draw.rect(GUI.SCREEN, self.menu_color, pygame.Rect(GUI.DISPLAY_X - GUI.MENU_SIZE, 0, 100, 700))
 
             if self.light_button.is_on and self.light_button.draw(GUI.SCREEN):
-
                 self.light_button.turn_off()
                 self.change_light()
             if self.light_button.draw(GUI.SCREEN):
                 self.light_button.turn_on()
                 self.change_light()
-
+            
             # Cell button
             self.button_navigate(self.cell_button)
 
@@ -238,14 +225,20 @@ class GUI:
             if self.play_button.draw(GUI.SCREEN):
                 self.play_button.turn_on()
                 print("Evolution starts")
+            
+            if self.erase_button.is_on and self.erase_button.draw(GUI.SCREEN):
+                self.erase_button.turn_off()
+            if self.erase_button.draw(GUI.SCREEN):
+                self.erase_button.turn_on()
+                print("Erase")
 
             # Quit button
             if self.quit_button.draw(GUI.SCREEN):
-                run = False
+                self.run = False
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:  # if press close button
-                    run = False
+                    self.run= False
 
             clock.tick(FPS)
 
@@ -257,10 +250,6 @@ class GUI:
             pygame.display.update()
 
 
-
-if __name__ == "__main__":
-    import environment
-
-    env = environment.Environment(40, 30)
-    display = GUI(env)
+if __name__ == '__main__':
+    display = GUI()
     display.main()
