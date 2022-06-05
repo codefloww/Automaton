b2a7265b680d1f533462a4d50693b3215809ae99
Metro <67468470+Dima-Vas@users.noreply.github.com>
2022-06-05 22:52:59 +0300

    Add files via upload

--- automata.py
+++ automata.py
@@ -1,20 +1,15 @@
 """module for discrete evolving automata"""
-from math import sqrt
-import random
-from environment import Environment
 
+"""module for discrete evolving automata"""
 
 from math import sqrt, floor
 import random
-from environment import Environment
-from cell import Cell
-
+from cell import Cell, Plant, Organism, Wall
 class Automata:
-    def __init__(self, cell, env) -> None:
+    def __init__(self, cell, env, genome = None) -> None:
         self.GENOME_SIZE = 8
-
-        self.genome = "0" * (2 * self.GENOME_SIZE)
-        # self.health = 10
+        self.genome = "".join([random.choice(["0", "1"]) for\
+             x in range(2*self.GENOME_SIZE)]) if genome == None else genome
         self.age = 0
         self.energy = 4
         self.cell = cell
@@ -25,38 +20,13 @@ class Automata:
     def __str__(self) -> str:
         return f"{self.get_genome()} - {self.x}, {self.y}"
 
-    
     def make_move(self):
         self._behavior_decider()
 
     def _abilities_decider(self) -> None:
-        abilities_namings = [self.see_ability, self.move_ability, self.kill_ability, self.eat_ability, self.hybernate_ability, self.photosynth_ability, self.reproduce_ability,self.produce_ability]
+        abilities_namings = [self.see_ability, self.move_ability, self.kill_ability, self.eat_ability, self.photosynth_ability, self.hybernate_ability, self.cross_ability, self.produce_ability]
         abilities_strength = list(map(lambda x: int(self.genome[x:x+2], 2), range(0, self.GENOME_SIZE*2, 2)))
         abilities = {abilities_namings[i]: abilities_strength[i] for i in range(len(abilities_namings))}
-
-
-    def _abilities_decider(self) -> None:
-        abilities_namings = [
-            self.see_ability,
-            self.move_ability,
-            self.eat_ability,
-            self.kill_ability,
-            self.hybernate_ability,
-            self.photosynth_ability,
-            self.reproduce_ability,
-            self.produce_ability,
-        ]
-        abilities_strength = list(
-            map(
-                lambda x: int(self.genome[x : x + 2], 2),
-                range(0, self.GENOME_SIZE * 2, 2),
-            )
-        )
-        abilities = {
-            abilities_namings[i]: abilities_strength[i]
-            for i in range(len(abilities_namings))
-        }
-
         return abilities
 
     def _behavior_decider(self) -> None:
@@ -64,7 +34,6 @@ class Automata:
         completed_action = False
         for ability in abilities:
             if abilities[ability] > 0:
-
                 completed_action = ability(abilities[ability])
             if completed_action == True:
                 break
@@ -72,11 +41,11 @@ class Automata:
     def move_ability(self, strength) -> None:
         surr = self.see_ability(self._abilities_decider()[self.see_ability])
 
-
-        def move_away(): # змушує автомат лівнути зі своєї клітинки і перейти в іншу, і, відповідно, глобально змінює грід. 
-            self.cell.organism = None
+        def move_away(): 
+            self.env[self.cell.x][self.cell.y] = Cell(self.x, self.y, 'empty', self.cell.light)
             self.cell = self.env.get_cell(self.x, self.y)
-            self.env.get_cell(self.x, self.y).organism = self
+            self.env[self.x][self.y]= Organism(self.x, self.y, self.cell.light)
+            self.env[self.x][self.y].set_organism(self)
             self.energy -= 1
 
         def get_possible_moves():
@@ -91,21 +60,17 @@ class Automata:
                         except IndexError:
                             pass
             return possible_moves
-        
-        def get_all_ranges(possible_moves, track_list): # розраховує відстань до кожного об'єкту зі списку track_list, яка буде між автоматом і об'єктом якщо він переміститься на якийсь з можливих селлів. Формат аутпуту функції : "x_possible_to_go y_possible_to_go" : [(Cell(x_possible_to_go, y_possible_to_go), range_to_an object1), ...] 
+
+        def get_all_ranges(possible_moves, track_list): # розраховує відстань до кожного об'єкту зі списку track_list, яка буде між автоматом і об'єктом якщо він переміститься на якийсь з можливих селлів. Формат аутпуту функції : "x_possible_to_go y_possible_to_go" : [(Cell(x_possible_to_go, y_possible_to_go), range_to_an object1), ...]
             ranges = []
             for i in track_list:
                 ranges.append([(cell, sqrt(abs(cell.x - i.x) + abs(cell.y - i.y))) for cell in possible_moves])
-
             possible_move_dict = {}
             for i in ranges:
                 for j in i:
                     if f"{j[0].x} {j[0].y}" not in possible_move_dict:
                         possible_move_dict[f"{j[0].x} {j[0].y}"] = []
                     possible_move_dict[f"{j[0].x} {j[0].y}"].append(j[1])
-
-            if len(possible_move_dict) == 0:
-                print(possible_moves, track_list)
             return possible_move_dict
 
         def escape(danger_cells): # визначає найкращі за векторним добутком координати для ВТЕЧІ і повертає їх у формі ("x", "y")
@@ -127,24 +92,22 @@ class Automata:
         def look_for_danger():
             return [x for x in surr if x.organism != None]
         def look_for_pray():
-            return [x for x in look_for_danger() if x.organism.energy <= self.energy] # замість self.energy підбиратимемо по силі бою, але зараз йой най буде
+            return [x for x in look_for_danger() if \
+                x.organism._abilities_decider()[x.organism.kill_ability] <= self._abilities_decider()[self.kill_ability]]
         def look_for_food():
             return [x for x in surr if x.cell_type == "plant"]
-
         danger_cells = look_for_danger()
         pray_cells = look_for_pray()
         food_cells = look_for_food()
 
 
-
 # Логіка - у пріоритеті напад на когось. Відразу ж за нападом йде втеча від небезпечного автоматона (напад на слабкого все одно вважається пріоритетом).
 # Не бачимо ворогів узагалі? Йдемо до їжі. Якщо ж навколо узагалі нічого немає - мігруємо у випадковому напрямку на випадкову відстань.
-# По факту - логіка агресора, але можна легко поміняти і взагалі ввести типи клітин по агресивності.
         try :
             if len(pray_cells) > 0:
                 self.x, self.y = (int(x) for x in move_towards(pray_cells).split(" "))
                 move_away()
-                
+
             elif len(danger_cells) > 0:
                 self.x, self.y = (int(x) for x in escape(danger_cells).split(" "))
                 move_away()
@@ -163,27 +126,46 @@ class Automata:
         except AttributeError:
             pass # немає куди тікати
 
-
     def see_ability(self, strength) -> None:
         return self.env.get_neighbors(self.x, self.y, strength)
-
 
     def eat_ability(self, strength) -> None:# їсть якщо стоїть на клітинці з їжею або їжа є на сусідніх клітинках. cell_type клітинки змінюється на "empty" і автомату додається енергія.
         to_eat = [x for x in self.see_ability(1) if x.cell_type == "plant"]
         to_eat.append(self.cell) if self.cell.cell_type == "plant" else 1
         if len(to_eat) > 0:
-            random.choice(to_eat).cell_type = "empty"
+            eaten_plant = random.choice(to_eat)
+            eaten_plant = Cell(eaten_plant.x, eaten_plant.y, 'empty', eaten_plant.light)
             self.energy += strength*5 if self.energy + strength*5 <= 50 else 50 - self.energy
             return True
         else :
             return False
 
+    def cross_ability(self, strength) -> None:
+        """changed to cross_ability"""
+        nearest_cells = [x for x in self.env.get_neighbors(self.x, self.y) if x.organism != None]
+        for cell in nearest_cells:
+            if strength == 0:
+                if abs(self.x-cell.x) == 1 and abs(self.y-cell.y) == 1:
+                    chosen_cell = cell
+                    break
+            elif strength == 1:
+                if (self.x-cell.x) == 0 or (self.y-cell.y) == 0:
+                    chosen_cell = cell
+                    break
+            else:
+                chosen_cell = cell
+                break
+        else:
+            return False
+        self, chosen_cell.organism = self.crossover(chosen_cell.organism)
+        if_mutate = random.choice([0, 1, 2])
+        if if_mutate == 0:
+            self.mutate()
+        self.energy -= 20
+        return True
 
-    def reproduce_ability(self, strength) -> None:
-        pass
 
     def kill_ability(self, strength) -> None:
-
         """
         can kill if other.kill_ability < strength
         """
@@ -206,83 +188,88 @@ class Automata:
                     else:
                         kill_probability = -1
                     if kill_probability == 1:
-                        self.env.killed_before.append(enemy)
                         self.energy += 20 if self.energy <= 30 else 50-self.energy
-                        enemy.cell.organism = None
+
+                        cell = Cell(self.x, self.y, 'empty', self.cell.light)
                         return True
-                    if kill_probability != -1:
+                    elif kill_probability == 0:
+                        return True
+                    elif kill_probability != -1:
                         break
-            else:
-                return False
+            return False
 
     def photosynth_ability(self, strength) -> None:
-        self.energy += floor(strength*1.5) if self.energy + floor(strength*1.5) <= 50 else 50 - self.energy
-        return True
-
+        self.energy += floor(strength*self.cell.light/2) if self.energy + floor(strength*1.5) <= 50 else 50 - self.energy
+        return False
 
     def produce_ability(self, strength) -> None:
-        self
+        diff_energy = self.energy - 35
+        if diff_energy < 5:
+            return False
+        number_of_child = 0
+        # number_of_plants = 0
+        if strength <= 1:
+            number_of_plants = diff_energy // 10
+        elif strength == 2:
+            if diff_energy >= 10:
+                number_of_child = 1
+                number_of_plants = 0
+            else:
+                number_of_plants = diff_energy // 5
+        else:
+            if diff_energy >= 10:
+                number_of_child = 1
+                diff_energy -= 10
+            number_of_plants = diff_energy // 5
+        nearest_cells = self.env.get_neighbors(self.x, self.y)
+        if number_of_child == 0 and number_of_plants == 0:
+            return False
+        check = False
+        for cell in nearest_cells:
+            if cell.cell_type != "empty":
+                continue
+            if number_of_child != 0:
+                cell = Organism(cell.x, cell.y, cell.light)
+                cell.organism = Automata(cell, self.env, self.genome)
+                number_of_child -= 1
+                self.energy -= 10
+                check = True
+            if number_of_plants > 0:
+                self.env[cell.x][cell.y] = Plant(cell.x, cell.y, cell.light)
+                number_of_plants -= 1
+                check = True
+            if number_of_plants == 0 and number_of_plants == 0:
+                self.energy = min(35, self.energy)
+                return True
+        else:
+            if check:
+                return True
+            return False
 
     def hybernate_ability(self, strength) -> None:
-
-        if random.randint(0, 100) > strength*10 :
-            return self.photosynth_ability(self._abilities_decider()[self.photosynth_ability])
-        else:
+        if self.energy < 5:
             self.energy += strength if self.energy + strength <= 50 else 50 - self.energy
             return True
-
+        return False
 
     def mutate(self) -> None:
         mutation_position = random.randint(0, self.GENOME_SIZE)
         self.genome = self.genome[:mutation_position] + str(int(self.genome[mutation_position])^1) + self.genome[mutation_position+1:]
-        
+
     def crossover(self, other) -> None:
         if not isinstance(other, Automata):
             raise TypeError("other must be an Automata")
         if self.GENOME_SIZE != other.GENOME_SIZE:
             raise ValueError("other must have the same genome size")
         crossing = random.randint(0, self.GENOME_SIZE)
-
-        self.genome, other.genome = (
-            self.genome[0:crossing] + other.genome[crossing:],
-            other.genome[0:crossing] + self.genome[crossing:],
-        )
-
-
+        self.genome, other.genome = self.genome[0:crossing] + other.genome[crossing:], other.genome[0:crossing] + self.genome[crossing:]
         return self, other
 
     def get_genome(self) -> list:
         return self.genome
-
-
     def get_health(self) -> int:
         return self.health
-
-
     def get_age(self) -> int:
         return self.age
     def get_energy(self) -> int:
         return self.energy
-
-if __name__ == "__main__":
-    # Змінив метод __str__ класу Cell на оцей рядок : return "M" if self.organism != None else "_" для кращих результатів.
-    env = Environment(10, 10)
-    my_cell = env.get_cell(0, 0)
-    authomatas = []
-
-    for i in range(50):
-        _cell = Cell(random.randint(0, 8), random.randint(0, 8))
-        _cell.organism = Automata(_cell, env)
-        env.set_cell(_cell.x, _cell.y, _cell)
-        authomatas.append(_cell.organism)
-    for i in range(50) :
-        env.get_cell(random.randint(0, 9) ,random.randint(0, 9)).cell_type = "plant"
-    print(env)
-    for i in range(10):
-        for j in authomatas:
-            j.make_move()
-        print(env)
-        print()
-    print(env.killed_before)
-
-
